edd900e789bc40227911a149f3212306aeadbdf9
Israel Sunuwar <isunuwar@inctic.io>
2022-07-11 13:22:11 +0545

    basic setup, file structure updated

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+const App = () => {
+  return (
+    <div className='ap-wide'>
+      <Header />
+      <div className="ap-main">
+        Main
       </div>
-    );
-  }
+      <Footer />
+    </div>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='ap-max ap-header'>
+        header
+    </header>
+  )
+}
+
+export default Header
